1f7ee90b9083d0ee802ae58064958b6035aa253e
xianwang <wxwjj521804@gmail.com>
2021-06-07 12:01:41 +0800

    2021.6.07 modified

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Windows:
 https://www.cnblogs.com/wilson-5133/p/10717768.html
 
 # Step 2: Open autoSignIn.py and change your own username and password
+The number in the username and password array names corresponds to the flag value
 ![avatar](./img/1.png)
 # Step 3: Run autoSignIn.py in cmd or terminal
-![avatar](./img/2.png)
+# Step 4: If you have a server, you can use crontab to auto sign in
+1. vi /etc/crontab
+2. ![avatar](./img/2.png)
--- autoSignIn.py
+++ autoSignIn.py
@@ -1,58 +1,79 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome()
-driver.get("https://healthreport.zju.edu.cn/ncov/wap/default/index")
-# Input your own username and password in send_keys("?")
-driver.find_element_by_id("username").send_keys("xxxxxxx")
-driver.find_element_by_id("password").send_keys("xxxxxxx")
-driver.find_element_by_id("dl").click()
-time.sleep(10)
-try:
-    driver.find_element_by_xpath("//*[@name='sfzx']/div/div[1]")
-except NoSuchElementException:
+def autosign(username,password,flag):
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('blink-settings=imagesEnabled=false')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=chrome_options)
+
+    driver.get("https://healthreport.zju.edu.cn/ncov/wap/default/index")
+    driver.find_element_by_id("username").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_id("dl").click()
+    time.sleep(10)
+    try:
+        driver.find_element_by_xpath("//*[@name='sfzx']/div/div[1]")
+    except NoSuchElementException:
+        time.sleep(5)
+
+    driver.find_element_by_xpath("//*[@name='sfbyjzrq']/div/div[5]").click()
+    # flag: 1: vaccinated; 2: not vaccinated; 3: one stitch
+    if(flag == 1):
+        driver.find_element_by_xpath("//*[@name='jzxgymqk']/div/div[2]").click()
+    elif(flag == 2):
+        driver.find_element_by_xpath("//*[@name='jzxgymqk']/div/div[3]").click()
+    elif(flag == 3):
+        driver.find_element_by_xpath("//*[@name='jzxgymqk']/div/div[1]").click()
+    # Are you on campus today
+    driver.find_element_by_xpath("//*[@name='sfzx']/div/div[1]").click()
+    # No
+    # driver.find_element_by_xpath("//*[@name='sfzx']/div/div[2]").click()
+
+    driver.find_element_by_xpath("//*[@name='area']/input").click()
+    time.sleep(15)
+    try:
+        driver.find_element_by_xpath("//*[@id='wapat']/div/div[2]/div").click()
+        Select(driver.find_element_by_xpath("//*[@name='ip']/div/div/select[1]")).select_by_visible_text("浙江省")
+        time.sleep(1)
+        Select(driver.find_element_by_xpath("//*[@name='ip']/div/div/select[2]")).select_by_visible_text("杭州市")
+        time.sleep(1)
+        Select(driver.find_element_by_xpath("//*[@name='ip']/div/div/select[3]")).select_by_visible_text("西湖区")
+    except NoSuchElementException:
+        print("Location permission is allowed")
+
+    driver.find_element_by_xpath("//*[@name='sfymqjczrj']/div/div[2]").click()
+    driver.find_element_by_xpath("//*[@name='sfqrxxss']").click()
+    driver.find_element_by_xpath("//*[@class='footers']").click()
     time.sleep(5)
+    try:
+        driver.find_element_by_xpath("//*[@class='wapcf-btn wapcf-btn-ok']").click()
+        print(username + " sign in successfully")
+    except NoSuchElementException:
+        print(username + " has already signed in")
+    driver.close()
 
-# Are you on campus today
-# Yes
-driver.find_element_by_xpath("//*[@name='sfzx']/div/div[1]").click()
-# No
-# driver.find_element_by_xpath("//*[@name='sfzx']/div/div[2]").click()
+username1 = []
+password1 = []
+username2 = []
+password2 = []
+username3 = []
+password3 = []
+# flag: 1: vaccinated; 2: not vaccinated; 3: one stitch
+flag = 1
+for i,j in zip(username1, password1):
+    autosign(i,j,flag)
 
-# Your location
-driver.find_element_by_xpath("//*[@name='area']/input").click()
-time.sleep(15)
-try:
-    # Close pop-up
-    driver.find_element_by_xpath("//*[@id='wapat']/div/div[2]/div").click()
-    # Select your location
-    Select(driver.find_element_by_xpath("//*[@name='ip']/div/div/select[1]")).select_by_visible_text("浙江省")
-    time.sleep(1)
-    Select(driver.find_element_by_xpath("//*[@name='ip']/div/div/select[2]")).select_by_visible_text("杭州市")
-    time.sleep(1)
-    Select(driver.find_element_by_xpath("//*[@name='ip']/div/div/select[3]")).select_by_visible_text("西湖区")
-except NoSuchElementException:
-    print("Location permission is allowed")
+flag = 2
+for i,j in zip(username2, password2):
+    autosign(i,j,flag)
 
-# Have your family members (including other close contact persons）entered Chinese Mainland 
-# over the past 14 days or plan to enter Chinese Mainland in 14 days
-# Yes
-# driver.find_element_by_xpath("//*[@name='sfymqjczrj']/div/div[1]").click()
-# No
-driver.find_element_by_xpath("//*[@name='sfymqjczrj']/div/div[2]").click()
-
-# Promise
-driver.find_element_by_xpath("//*[@name='sfqrxxss']").click()
-# Submit info
-driver.find_element_by_xpath("//*[@class='footers']").click()
-time.sleep(5)
-#Submit
-try:
-    driver.find_element_by_xpath("//*[@class='wapcf-btn wapcf-btn-ok']").click()
-    print("Sign in successfully")
-except NoSuchElementException:
-    print("You have already signed in")
-
-driver.close()
+flag = 3
+for i,j in zip(username3, password3):
+    autosign(i,j,flag)
